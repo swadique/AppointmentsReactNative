3d5e40a12052d5f074964a8b0740ad17a6011313
swadique <swadique@dexlock.com>
2021-02-19 12:26:53 +0530

    Saved

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {ActivityIndicator} from 'react-native';
-import jwtDecode from 'jwt-decode';
 import Login from './src/modules/register/login';
 import Signup from './src/modules/register/signup';
 import UserContext from './src/contexts/userContext';
@@ -14,32 +13,33 @@ import checkTokenValidity from './src/util/checkTokenvalidity';
 import AuthContext from './src/contexts/authContext';
 import Toast from 'react-native-toast-message';
 import Appointments from './src/modules/appointments';
+import {Icon} from 'react-native-elements';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Sellers from './src/modules/sellers';
+
 const App = () => {
   const [userData, setUserData] = React.useState(
     LocalStorage.userData.getItem() || {},
   );
-  const [authToken, setAuthToken] = React.useState(
-    LocalStorage.authToken.getItem() || {},
-  );
+  const [authToken, setAuthToken] = React.useState();
   const [isValidToken, setIsValidToken] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
   const Stack = createStackNavigator();
   const Tab = createBottomTabNavigator();
 
   React.useEffect(() => {
-    checkTokenValidity(authToken).then(({status, type}) => {
+    checkTokenValidity().then(({status, type}) => {
       console.log(status, type);
       if (status) {
         setLoading(false);
         setIsValidToken(true);
-        Toast.show({ text1: type});
+        Toast.show({text1: type});
       } else {
         setLoading(false);
         setIsValidToken(false);
-        Toast.show({ text1: type});
+        Toast.show({text1: type});
       }
     });
-    console.log(authToken);
   }, [authToken]);
 
   function updateUserData(data) {
@@ -52,56 +52,86 @@ const App = () => {
   }
   return (
     <>
-      <UserContext.Provider
-        value={{
-          userData: userData,
-          setUserData: updateUserData,
-        }}>
-        <AuthContext.Provider
+      <SafeAreaProvider>
+        <UserContext.Provider
           value={{
-            authToken: authToken,
-            setAuthToken: updateAuthToken,
+            userData: userData,
+            setUserData: updateUserData,
           }}>
-          <Toast ref={(ref) => Toast.setRef(ref)} />
-          <NavigationContainer>
-            {loading ? (
-              <View
-                style={{
-                  justifyContent: 'center',
-                  flex: 1,
-                }}>
-                <ActivityIndicator size="large" color="#0000ff" />
-              </View>
-            ) : isValidToken ? (
-              <Tab.Navigator initialRouteName="login">
-                <Tab.Screen
-                  name="home"
-                  component={Home}
-                  options={{title: 'Home'}}
-                />
-                <Tab.Screen
-                  name="appointments"
-                  component={Appointments}
-                  options={{title: 'Appointments'}}
-                />
-              </Tab.Navigator>
-            ) : (
-              <Stack.Navigator initialRouteName="login">
-                <Stack.Screen
-                  name="login"
-                  component={Login}
-                  options={{title: 'Login'}}
-                />
-                <Stack.Screen
-                  name="signup"
-                  component={Signup}
-                  options={{title: 'Login'}}
-                />
-              </Stack.Navigator>
-            )}
-          </NavigationContainer>
-        </AuthContext.Provider>
-      </UserContext.Provider>
+          <AuthContext.Provider
+            value={{
+              authToken: authToken,
+              setAuthToken: updateAuthToken,
+            }}>
+            <Toast ref={(ref) => Toast.setRef(ref)} />
+            <NavigationContainer>
+              {loading ? (
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    flex: 1,
+                  }}>
+                  <ActivityIndicator size="large" color="#0000ff" />
+                </View>
+              ) : isValidToken ? (
+                <Tab.Navigator
+                  initialRouteName="login"
+                  screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, color, size}) => {
+                      let iconName;
+
+                      if (route.name === 'Home') {
+                        iconName = focused
+                          ? 'ios-information-circle'
+                          : 'ios-information-circle-outline';
+                      } else if (route.name === 'Settings') {
+                        iconName = focused ? 'ios-list-box' : 'ios-list';
+                      }
+                      return (
+                        <>
+                          <Icon nname="g-translate" color="#000" />
+                        </>
+                      );
+                    },
+                  })}
+                  tabBarOptions={{
+                    activeTintColor: 'blue',
+                    inactiveTintColor: 'gray',
+                  }}>
+                  <Tab.Screen
+                    name="home"
+                    component={Home}
+                    options={{title: 'Home'}}
+                  />
+                  <Tab.Screen
+                    name="appointments"
+                    component={Appointments}
+                    options={{title: 'Appointments'}}
+                  />
+                  <Tab.Screen
+                    name="sellers"
+                    component={Sellers}
+                    options={{title: 'Sellers'}}
+                  />
+                </Tab.Navigator>
+              ) : (
+                <Stack.Navigator initialRouteName="login">
+                  <Stack.Screen
+                    name="login"
+                    component={Login}
+                    options={{title: 'Login'}}
+                  />
+                  <Stack.Screen
+                    name="signup"
+                    component={Signup}
+                    options={{title: 'Login'}}
+                  />
+                </Stack.Navigator>
+              )}
+            </NavigationContainer>
+          </AuthContext.Provider>
+        </UserContext.Provider>
+      </SafeAreaProvider>
     </>
   );
 };
